def7b1079e61163bf39ba4558e83930d2b8f1b7c
Jacob Tomczeszyn <jake.tomczeszyn@gmail.com>
2025-02-05 17:03:22 -0600

    Removed Build and Devel from repo

--- light_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dylan/Contact_Jacobian/light_ws/src
-# Build directory: /home/dylan/Contact_Jacobian/light_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("trikey")
--- light_ws/build/trikey/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;rospy;std_msgs;gazebo_ros".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-ltrikey".split(';') if "-ltrikey" != "" else []
-PROJECT_NAME = "trikey"
-PROJECT_SPACE_DIR = "/home/dylan/Contact_Jacobian/light_ws/install"
-PROJECT_VERSION = "0.0.0"
